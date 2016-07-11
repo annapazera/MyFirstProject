52eba55a1fa690878d9cb289779dada265a9809a
Anna Pazera <anna.pazera@tlen.pl>
2016-07-11 12:41:42 +0200

    MOJA DRUGA WERSJA

--- src/main/java/WitajSwiecie.java
+++ src/main/java/WitajSwiecie.java
@@ -3,7 +3,7 @@ public class WitajSwiecie
 {
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		System.out.println("Witaj Œwiecie !");
+	
+		System.out.println("Witaj Piêkny Œwiecie  !");
 	}
 }
