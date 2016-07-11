f5c3b5127cc0478a582cb442cf3054e40b8df75e
Anna Pazera <anna.pazera@tlen.pl>
2016-07-11 13:38:36 +0200

    Web aplikacja

--- src/main/java/WitajSwiecie.java
+++ src/main/java/WitajSwiecie.java
@@ -1,9 +1,11 @@
+import spark.Spark;
 
 public class WitajSwiecie
 {
 	public static void main(String[] args) 
 	{
 	
+		Spark.get("/Hello",(req, res) -> "Siema");
 		System.out.println("Witaj Piêkny wiecie  !");
 		System.out.println("Witaj ęą!");
 	}
