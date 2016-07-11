3b07d5c10101f21a04955e1228409922aaa796a6
Anna Pazera <anna.pazera@tlen.pl>
2016-07-11 14:12:53 +0200

    html wysylany do przegladarki

--- src/main/java/WitajSwiecie.java
+++ src/main/java/WitajSwiecie.java
@@ -5,7 +5,7 @@ public class WitajSwiecie
 	public static void main(String[] args) 
 	{
 	
-		Spark.get("/Hello",(req, res) -> "Siema");
+		Spark.get("/Hello",(req, res) -> "<html><h1>Siema</h1>traaalalala</html>");
 		System.out.println("Witaj Piêkny wiecie  !");
 		System.out.println("Witaj ęą!");
 	}
