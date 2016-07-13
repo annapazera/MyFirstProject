f2ba4e6e63e1077fbf28045b511f8614f3d1bc2b
Anna Pazera <anna.pazera@tlen.pl>
2016-07-13 19:19:23 +0200

    zmiany

--- src/main/java/WitajSwiecie.java
+++ src/main/java/WitajSwiecie.java
@@ -4,7 +4,8 @@ public class WitajSwiecie
 {
 	public static void main(String[] args) 
 	{
-	
+	      Spark.staticFileLocation("/public");
+
 		Spark.get("/Hello",(req, res) -> "<html><h1>Siema</h1>traaalalala</html>");
 		System.out.println("Witaj Piêkny wiecie  !");
 		System.out.println("Witaj ęą!");
