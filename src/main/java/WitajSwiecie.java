b93c02988fff99086ae2f4bf567555f6baa2ac18
Piotr Przybylak <piotr.przybylak@gmail.com>
2016-07-14 10:21:57 +0200

    Freemarker example

--- src/main/java/WitajSwiecie.java
+++ src/main/java/WitajSwiecie.java
@@ -1,13 +1,20 @@
+import org.omg.CORBA.FREE_MEM;
+import spark.ModelAndView;
 import spark.Spark;
+import spark.template.freemarker.FreeMarkerEngine;
 
-public class WitajSwiecie
-{
-	public static void main(String[] args) 
-	{
-	      Spark.staticFileLocation("/public");
+public class WitajSwiecie {
+    public static void main(String[] args) {
 
-		Spark.get("/Hello",(req, res) -> "<html><h1>Siema</h1>traaalalala</html>");
-		System.out.println("Witaj Piękny świecie  !");
-		System.out.println("Witaj ęą!");
-	}
+        Spark.staticFileLocation("/public");
+
+        Spark.get("/Hello", (req, res) -> "<html><h1>Siema</h1>traaalalala</html>");
+
+        Spark.get("/travel", (res, req) -> {
+            return new ModelAndView(null,"ani_stronka.ftl" );
+        }, new FreeMarkerEngine());
+
+        System.out.println("Witaj Piękny świecie  !");
+        System.out.println("Witaj ęą!");
+    }
 }
