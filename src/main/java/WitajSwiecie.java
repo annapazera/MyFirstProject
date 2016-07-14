475d12bfb0470e1c9e9f47eaa5f3c9c4c02d2f14
Anna Pazera <anna.pazera@tlen.pl>
2016-07-14 18:31:38 +0200

    bbbbb

--- src/main/java/WitajSwiecie.java
+++ src/main/java/WitajSwiecie.java
@@ -16,25 +16,35 @@ public class WitajSwiecie {
         Spark.get("/travel", (req, res) -> {
             String name = req.queryParams("name");
             if (name == null || "".equals(name)) {
-                name = "Stranger";
+                name = "";
+            }
+
+            String surname = req.queryParams("surname");
+            if (surname == null || "".equals(surname)) {
+                surname = "";
             }
             Map<String, Object> model = new HashMap<>();
             model.put("name", name);
+            model.put("surname", surname);
             return new ModelAndView(model,"ani_stronka.ftl" );
         }, new FreeMarkerEngine());
 
         Spark.post("/travel", (req, res) -> {
             String name = req.queryParams("name");
             if (name == null || "".equals(name)) {
-                name = "Stranger";
+                name = "";
+            }
+            String surname = req.queryParams("surname");
+            if (surname == null || "".equals(surname)) {
+                surname = "";
             }
             Map<String, Object> model = new HashMap<>();
             model.put("name", name);
-            return new ModelAndView(model,"ani_stronka.ftl" );
+            model.put("surname", surname);
+            return new ModelAndView(model, "ani_stronka.ftl");
         }, new FreeMarkerEngine());
-
 
         System.out.println("Witaj Piękny świecie  !");
         System.out.println("Witaj ęą!");
-    }
+        }
 }
