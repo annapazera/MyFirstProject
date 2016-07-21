415b571e6768850de224239557c8c562de6154c9
Anna Pazera <anna.pazera@tlen.pl>
2016-07-21 09:47:29 +0200

    kalkulator

---
+++ src/main/java/kalkulator.java
@@ -0,0 +1,31 @@
+/**
+ * Created by Anna Kacprzak on 2016-07-20.
+ */
+public class kalkulator {
+    public static double dodawaj(double a, double b){
+        double c;
+         c= a+b;
+        return c;
+
+    }
+
+public static double odejmuj (double d, double e){
+        double f;
+    f = d-e;
+    return f;
+    }
+
+    public static double mnoz (double g, double h){
+        double i;
+        i= g * h;
+        return i;
+
+    }
+    public static double dziel (double j, double k){
+        double l;
+        l= j/k;
+        return l;
+
+    }
+
+}
