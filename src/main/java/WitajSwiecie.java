623a786a2d941d27fc6b888ad0a6b80d63140211
Anna Pazera <anna.pazera@tlen.pl>
2016-07-21 10:46:56 +0200

    wyswietlenie imienia

--- src/main/java/WitajSwiecie.java
+++ src/main/java/WitajSwiecie.java
@@ -17,37 +17,35 @@ public class WitajSwiecie {
         Spark.get("/kalk", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             String Imię = req.queryParams("Imię");
-
-           
-
-
-
-        System.out.println(Imię + " podaj dwie dowolne liczby ");
-        double liczba1;
-        double liczba2;
-        Scanner input1 = new Scanner(System.in);
-        liczba1 = input1.nextDouble();
-        liczba2 = input1.nextDouble();
-        System.out.println(Imię + " twoje liczby to " + liczba1 + " i " + liczba2);
-
-        double wynik = kalkulator.dodawaj(liczba1, liczba2);
-        System.out.println("Suma Twoich liczb to " + wynik );
-        double wynik2 = kalkulator.odejmuj(liczba1, liczba2);
-        System.out.println ( "Wynik odejmowania Twoich liczb to " + wynik2);
-
-        double wynik3=kalkulator.mnoz(liczba1,liczba2);
-        System.out.println("Iloczyn Twoich liczb to " + wynik3 );
-
-        double wynik4=kalkulator.dziel(liczba1, liczba2);
-        System.out.println("Iloraz Twoich liczb to " + wynik4 );
+            return new ModelAndView(model, "KalkulatorAni.ftl");
+        }, new FreeMarkerEngine());
 
 
+//        System.out.println(Imię + " podaj dwie dowolne liczby ");
+//        double liczba1;
+//        double liczba2;
+//        Scanner input1 = new Scanner(System.in);
+//        liczba1 = input1.nextDouble();
+//        liczba2 = input1.nextDouble();
+//        System.out.println(Imię + " twoje liczby to " + liczba1 + " i " + liczba2);
+//
+//        double wynik = kalkulator.dodawaj(liczba1, liczba2);
+//        System.out.println("Suma Twoich liczb to " + wynik );
+//        double wynik2 = kalkulator.odejmuj(liczba1, liczba2);
+//        System.out.println ( "Wynik odejmowania Twoich liczb to " + wynik2);
+//
+//        double wynik3=kalkulator.mnoz(liczba1,liczba2);
+//        System.out.println("Iloczyn Twoich liczb to " + wynik3 );
+//
+//        double wynik4=kalkulator.dziel(liczba1, liczba2);
+//        System.out.println("Iloraz Twoich liczb to " + wynik4 );
 
-        Map<String, Object> model = new HashMap<>();
-        model.put("Imię", Imię);
-        model.put("Liczba1", liczba1);
-        model.put("Liczba2", liczba2);
-        return new ModelAndView(model, "KalkulatorAni.ftl");
+
+//                    Map<String, Object> model = new HashMap<>();
+//                    model.put("Imię", Imię);
+//        model.put("Liczba1", liczba1);
+//        model.put("Liczba2", liczba2);
+
 
 //        Spark.get("/Hello", (req, res) -> "<html><h1>Siema</h1>traaalalala</html>");
 //
@@ -79,9 +77,6 @@ public class WitajSwiecie {
 //        System.out.println("Witaj Piękny świecie  !");
 //        System.out.println("Witaj ęą!");
 
-    }
-
-
 
 //    private static ModelAndView dupa(Request req) {
 //        String name = req.queryParams("name");
@@ -102,8 +97,7 @@ public class WitajSwiecie {
 //        model.put("name", name);
 //        model.put("surname", surname);
 //        model.put("jakis_atrybut", 123);
-//        return new ModelAndView(model, "ani_stronka.ftl");
-//
-//
-//    }
-}
+//        return new ModelAndView(model, "ani_stronka.ftl");}
+
+
+        }}
